d3259ddb38e774c6500c67de1710bccf65f958bc
Damien Kihe <dkkihe@live.com>
2017-08-22 14:03:34 -1000

    Added more inputs

--- src/App.js
+++ src/App.js
@@ -15,10 +15,11 @@ class App extends Component {
         }
     }
     createArr(){
-        let tekken_re = /(\[(.*?)\]|\((.*?)\)|\~|\d\+\d\+\d\+\d|\d\+\d\+\d|\d\+\d|\d|\s|\+|\_|\,)/
-        let sf_re = /(\[(.*?)\]|\((.*?)\)|^\w+( \w+)*$|\s|\~|\+|\_|\,)/
+        let tekken_re = /(\[(.*?)\]|\((.*?)\)|\d\+\d\+\d\+\d|\d\+\d\+\d|\d\+\d|\d|\s|\+|\_|\,)/
+        let sf_re = /(\[(.*?)\]|\((.*?)\)|^\w+( \w+)*$|\s|\.|\+|\_|\,)/
         let userinput = this.state.term
         let arr = []
+        let replaceArr = []
         //Iterate through array and JSON; push if terms match
         //Use upper and lower case if game is tekken
         if (this.state.game === "tekken"){    
@@ -26,7 +27,7 @@ class App extends Component {
         } else {
             var item = userinput.toLowerCase().split(sf_re);
         }
-            for(var i=0;i<item.length;i++){
+        for(var i=0;i<item.length;i++){
             //If text is "[]" or "()" push to array
             if(/(\[(.*?)\]|\((.*?)\))/g.test(item[i])){
                 arr.push(item[i])
@@ -44,7 +45,7 @@ class App extends Component {
 
     createImage(arr){
         document.querySelector(".images").innerHTML = " "
-        for(var i=0;i<arr.length;i++){
+        for(var i=0;i<arr.length;i++){ //go through user input array
             //Create notes using "[]" or "()"
             if(/(\[(.*?)\]|\((.*?)\))/g.test(arr[i])){
                 let text = arr[i]
@@ -52,17 +53,28 @@ class App extends Component {
                 note.innerHTML = String(text)
                 document.querySelector(".images").appendChild(note)
             }
-            //Create images by coparing to array
-            for (var key in list[this.state.game]){
-                for(var j=0;j<list[this.state.game][key].term.length;j++){
-                    if (list[this.state.game][key].term[j] === arr[i]){
-                        for (var imgNum=0;imgNum<list[this.state.game][key].image.length;imgNum++){
+            //Create images by comparing to array
+            for (var key in list[this.state.game]){ //go through list for game
+                for(var j=0;j<list[this.state.game][key].term.length;j++){ //go through each term
+                    if (list[this.state.game][key].term[j] === arr[i]){ //compare user input array to term in game list
+                        for (var imgNum=0;imgNum<list[this.state.game][key].image.length;imgNum++){ //create each image in list image array
                             let img = document.createElement("img")
                             img.src = String(list[this.state.game][key].image[imgNum])
-                            //img.id = String(i)
-                            document.querySelector(".images").appendChild(img)
+                            document.querySelector(".images").appendChild(img).className = String(i)
                         }
                     }
+                }
+            }
+        }
+    }
+
+    replaceArr(arr){
+        let temp = []
+        for (var i=0;i<arr.length;i++){
+            for (var key in list[this.state.game]){
+                if (list[this.state.game][key].term[i] === arr[i] && list[this.state.game][key].type === "motion"){
+                    if (list[this.state.game][key].term[i] === arr[i-1] && list[this.state.game][key].type === "button")
+                        return true
                 }
             }
         }
@@ -77,14 +89,16 @@ class App extends Component {
                 <Translator 
                     valueProp={this.state.term}
                     inputProp={event => this.setState({term: event.target.value})}
-                    testProp={console.log(this.createArr())}
                     arrayProp={this.createArr()}
                 />
                 <Output 
                     imageProp={event => this.createImage(this.createArr())}
                 />
                 <div>
-                    {console.log(this.state.game)}
+                    {console.log(this.createArr())}
+                </div>
+                <div>
+                    {console.log(this.replaceArr(this.createArr()))}
                 </div>
             </div>
         );
