02eeff66165fbaef0069c82bc653a011d8a7f220
Damien Kihe <dkkihe@live.com>
2017-08-29 21:12:39 -1000

    Added translation text for images

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Translator from './Translator.js';
 import Output from './Output.js';
 import './App.css';
 const list = require("./list.json")
-
 class App extends Component {
     constructor(props){
         super(props)
@@ -15,7 +14,7 @@ class App extends Component {
         }
     }
     createArr(){
-
+        let altImg = document.getElementsByTagName("img")
         let tekken_re = /(\d\+\d\+\d\+\d|\d\+\d\+\d|\d\+\d|\d|\s|\+|\_|\,)/
         let sf_re = /(?:(,)\s+|\s+(xx)\s+)|\+|(,)/
         let userinput = this.state.term
@@ -42,6 +41,9 @@ class App extends Component {
                             for (var imgNum=0;imgNum<list[this.state.game][key].image.length;imgNum++){ 
                                 let img = document.createElement("img")
                                 img.src = String(list[this.state.game][key].image[imgNum])
+                                if(list[this.state.game][key].hasOwnProperty('alt')){
+                                    img.alt = String(list[this.state.game][key].alt[imgNum])
+                                }
                                 document.querySelector(".images").appendChild(img).className = String(i)
                             }
                         }
@@ -49,41 +51,29 @@ class App extends Component {
                 }
             }
         }
-    }
-
-    /*createImage(arr){
-        for(var i=0;i<arr.length;i++){ 
-            if(/(\[(.*?)\]|\((.*?)\))/g.test(arr[i]) && this.state.game === "tekken"){
-                let text = arr[i]
-                let note = document.createElement("span")
-                note.innerHTML = String(text)
-                document.querySelector(".images").appendChild(note)
-            }
-            for (var key in list[this.state.game]){ 
-                for(var j=0;j<list[this.state.game][key].term.length;j++){ 
-                    if (list[this.state.game][key].term[j] === arr[i]){ 
-                        for (var imgNum=0;imgNum<list[this.state.game][key].image.length;imgNum++){ 
-                            let img = document.createElement("img")
-                            img.src = String(list[this.state.game][key].image[imgNum])
-                            document.querySelector(".images").appendChild(img).className = String(i)
-                        }
-                    }
-                }
+        if (altImg){
+            for (var i = 0; i < altImg.length;i++){
+                altImg[i].addEventListener('click',function show(){
+                    var myAlt = this.alt;
+                    document.querySelector("#text").innerHTML = myAlt;
+                })
             }
         }
-    }*/
+    }
 
     render() {
         return (
             <div>
+                <div id="title">fg translator</div>
                 <Games 
                     gameProp={event => this.setState({game: event.target.value})}
                 />
                 <Translator 
                     valueProp={this.state.term}
                     inputProp={event => this.setState({term: event.target.value})}
-                    //arrayProp={this.createArr()}
+                    
                 />
+                <div id="text">click an image to see a translation</div>
                 <Output
                     imageProp={this.createArr()}
                 />
