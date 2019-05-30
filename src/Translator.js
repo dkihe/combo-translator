8e54434be05a7427c0e39d28ab7ad498739bcaf3
dkihe <dnkkihe@gmail.com>
2019-05-29 15:23:38 -1000

    Added SamSho profiles

--- src/Translator.js
+++ src/Translator.js
@@ -3,11 +3,12 @@ import {CopyToClipboard} from 'react-copy-to-clipboard';
 import { Grid, Label, Select, Button, Icon, Input, Container, Dropdown } from 'semantic-ui-react';
 
 const options = [
-  { key: 'streetfighter', text: 'Street Fighter', value: 'streetfighter' },
-  { key: 'tekken', text: 'Tekken', value: 'tekken' },
+	{ key: 'streetfighter', text: 'Street Fighter', value: 'streetfighter' },
+	{ key: 'tekken', text: 'Tekken', value: 'tekken' },
 	{ key: 'mvci', text: 'Marvel vs Capcom: Infinite ', value: 'mvci' },
 	{ key: 'dbfz', text: 'Dragon Ball FighterZ', value: 'dbfz' },
 	{ key: 'bbtag', text: 'BlazBlue Cross Tag Battle', value: 'bbtag' },
+	{ key: 'samsho', text: 'Samurai Shodown', value: 'samsho' }
 ]
 
 class Translator extends Component{
