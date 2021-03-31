c6aef3a4211055b18b9477014ed55e614172efce
dkihe <dnkkihe@gmail.com>
2021-03-30 14:13:14 -1000

    Added temp new images for states

--- src/GameDropdown.js
+++ src/GameDropdown.js
@@ -9,10 +9,10 @@ const options = [
 	{ key: "mvci", text: "Marvel vs Capcom: Infinite ", value: "mvci" },
 	{ key: "dbfz", text: "Dragon Ball FighterZ", value: "dbfz" },
 	{ key: "bbtag", text: "BlazBlue Cross Tag Battle", value: "bbtag" },
-	{ key: "samsho", text: "Samurai Shodown", value: "samsho" },
+	// { key: "samsho", text: "Samurai Shodown", value: "samsho" },
 	{ key: "unib", text: "Under Night In-Birth", value: "unib" },
-	{ key: "mk", text: "Mortal Kombat 11", value: "mk" },
-	{ key: "sc", text: "Soul Calibur 6", value: "sc" },
+	// { key: "mk", text: "Mortal Kombat 11", value: "mk" },
+	// { key: "sc", text: "Soul Calibur 6", value: "sc" },
 ];
 
 // Material UI styling
